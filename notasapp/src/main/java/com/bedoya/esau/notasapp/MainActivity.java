752e8f6bfcc46b0a3b865ebe8fb1c63ca9ed905f
esau.bedoya <esau.bedoya@udea.edu.co>
2016-04-03 13:36:16 -0500

    final_1

--- notasapp/src/main/java/com/bedoya/esau/notasapp/MainActivity.java
+++ notasapp/src/main/java/com/bedoya/esau/notasapp/MainActivity.java
@@ -8,12 +8,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText eExpo, ePrac, eProy, eNota;
-    Button bCalc;
+    Button bCalc, bClean;
+    TextView tError;
+    Double exp=30.0, pra=50.0, pro=20.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +28,52 @@ public class MainActivity extends AppCompatActivity {
         eProy=(EditText)findViewById(R.id.eProy);
         eNota=(EditText)findViewById(R.id.eNotaf);
         bCalc=(Button)findViewById(R.id.bCalcular);
+        tError=(TextView)findViewById(R.id.tError);
+        bClean=(Button)findViewById(R.id.bLimpiar);
+
+        eExpo.setHint(exp.toString() + "%");
+        ePrac.setHint(pra.toString() + "%");
+        eProy.setHint(pro.toString() + "%");
 
         bCalc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                double nota;
+                double nexpo, npra, npro, nota;
 
-                nota=Double.parseDouble(eExpo.getText().toString())*0.15+
-                        Double.parseDouble(ePrac.getText().toString())*0.5+
-                        Double.parseDouble(eProy.getText().toString())*0.35;
-                eNota.setText(String.valueOf(nota));
+                if (eExpo.getText().toString().length() == 0 || eProy.getText().toString().length() == 0 || ePrac.getText().toString().length() == 0)
+                    tError.setText("Faltan Datos");
+                else {
+                    nexpo = Double.parseDouble(eExpo.getText().toString());
+                    npra = Double.parseDouble(ePrac.getText().toString());
+                    npro = Double.parseDouble(eProy.getText().toString());
+                    if (nexpo < 0 || nexpo > 5 || npra < 0 || npra > 5 || npro < 0 || npro > 5)
+                        tError.setText("Algunos valores están fuera del rango.");
+                    else {
+                        nota = Math.rint(((nexpo * exp + npra * pra + npro * pro) / 100) * 10) / 10;
+                        eNota.setText(Double.toString(nota));
+                        tError.setText("");
+
+                    }
+
+                }
+
             }
         });
+
+        bClean.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ePrac.setText("");
+                eProy.setText("");
+                eExpo.setText("");
+                eNota.setText("");
+                eExpo.setHint(exp.toString() + "%");
+                ePrac.setHint(pra.toString() + "%");
+                eProy.setHint(pro.toString() + "%");
+                tError.setText("");
+            }
+        });
+
     }
 
     @Override
@@ -51,9 +88,9 @@ public class MainActivity extends AppCompatActivity {
         if(id==R.id.mConfigurar) {
             // Toast.makeText(this,"Presionó configurar", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(this, SettingsActivity.class);
-            intent.putExtra("pExpo",15);
-            intent.putExtra("pPrac",50);
-            intent.putExtra("pProy",35);
+            intent.putExtra("pExpo",exp);
+            intent.putExtra("pPrac",pra);
+            intent.putExtra("pProy",pro);
             startActivityForResult(intent, 1234);
         }
         return super.onOptionsItemSelected(item);
@@ -61,12 +98,22 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        String exp, pra, pro;
+        String sexp,spra,spro;
+
         if(requestCode==1234 && resultCode==RESULT_OK){
-            exp=data.getExtras().getString("npExp");
-            pra=data.getExtras().getString("npPrac");
-            pro=data.getExtras().getString("npProy");
-            Toast.makeText(this, "Nuevos datos: Expo: "+exp+" Prac: "+pra+" Proy: "+pro,Toast.LENGTH_SHORT).show();
+            sexp=data.getExtras().getString("npExp");
+            spra=data.getExtras().getString("npPrac");
+            spro=data.getExtras().getString("npProy");
+            Toast.makeText(this, "Nuevos datos: Expo: "+sexp+" Prac: "+spra+" Proy: "+spro,Toast.LENGTH_SHORT).show();
+            exp=Double.parseDouble(sexp);
+            pra=Double.parseDouble(spra);
+            pro=Double.parseDouble(spro);
+            eExpo.setText("");
+            eProy.setText("");
+            ePrac.setText("");
+            eExpo.setHint(sexp + "%");
+            ePrac.setHint(spra+ "%");
+            eProy.setHint(spro+ "%");
         }
     }
 }
--- notasapp/src/main/java/com/bedoya/esau/notasapp/SettingsActivity.java
+++ notasapp/src/main/java/com/bedoya/esau/notasapp/SettingsActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
 
     EditText epExpo, epPrac, epProy;
-    Button bGuardar;
+    Button bGuardar, bClean2;
+    TextView tErrPor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,22 +23,45 @@ public class SettingsActivity extends AppCompatActivity {
         epPrac=(EditText)findViewById(R.id.epPrac);
         epProy=(EditText)findViewById(R.id.epProy);
         bGuardar=(Button)findViewById(R.id.bGuarad);
+        tErrPor=(TextView)findViewById(R.id.tErPor);
+        bClean2=(Button)findViewById(R.id.bLimPor);
 
         Bundle extras=getIntent().getExtras();
 
-        epExpo.setText(String.valueOf(extras.getInt("pExpo")));
-        epProy.setText(String.valueOf(extras.getInt("pProy")));
-        epPrac.setText(String.valueOf(extras.getInt("pPrac")));
+        epExpo.setText(String.valueOf(extras.getDouble("pExpo")));
+        epProy.setText(String.valueOf(extras.getDouble("pProy")));
+        epPrac.setText(String.valueOf(extras.getDouble("pPrac")));
 
         bGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent();
-                intent.putExtra("npExp",epExpo.getText().toString());
-                intent.putExtra("npPrac",epPrac.getText().toString());
-                intent.putExtra("npProy",epProy.getText().toString());
-                setResult(RESULT_OK, intent);
-                finish();
+
+                if (epPrac.getText().toString().length() == 0 || epProy.getText().toString().length() == 0 || epExpo.getText().toString().length() == 0)
+                    tErrPor.setText("Faltan Datos");
+                else if ((Double.parseDouble(epExpo.getText().toString()) + Double.parseDouble(epPrac.getText().toString()) +
+                        Double.parseDouble(epProy.getText().toString())) != 100)
+                    tErrPor.setText("El porcentaje no es igual a 100%");
+                else {
+                    tErrPor.setText("");
+                    Intent intent = new Intent();
+                    intent.putExtra("npExp", epExpo.getText().toString());
+                    intent.putExtra("npPrac", epPrac.getText().toString());
+                    intent.putExtra("npProy", epProy.getText().toString());
+                    setResult(RESULT_OK, intent);
+                    finish();
+
+                }
+
+            }
+        });
+        bClean2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                epPrac.setText("");
+                epProy.setText("");
+                epExpo.setText("");
+                tErrPor.setText("");
+
             }
         });
     }
